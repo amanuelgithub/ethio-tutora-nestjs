830512d1e64dcc5488ca0455e6d7ddd59f1f2abf
amanuelgithub <amanuelgirma070@gmail.com>
2022-07-23 20:31:49 +0300

    deployment

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -18,7 +18,7 @@ const postgresqlDataSourceOption: DataSourceOptions = {
   type: 'postgres',
   // host: process.env.DATABASE_HOST || 'localhost',
   // port: process.env.DATABASE_PORT || 5432,
-  // database: process.env.DATABASE_NAME || 'ethio_tutora',
+  database: process.env.DATABASE_NAME || 'ethio_tutora',
   // username: process.env.DATABASE_USER || 'postgres',
   // password: process.env.DATABASE_PASSWORD || 'password',
   ssl: {
